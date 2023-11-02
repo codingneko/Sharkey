8e1c50e20a907771f0c360b958c50196b0d0508e
CodingNeko <codingneko@gmail.com>
2023-11-02 09:51:06 +0100

    ROUND AVATARS!

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -299,7 +299,7 @@ export const defaultStore = markRaw(new Storage('base', {
 	},
 	squareAvatars: {
 		where: 'device',
-		default: true,
+		default: false,
 	},
 	showAvatarDecorations: {
 		where: 'device',
